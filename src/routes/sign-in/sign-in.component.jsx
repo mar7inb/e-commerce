ec8035cc10f9ec5cedfb78b35d2d43af57a203ca
unknown <mbp818@gmail.com>
2022-10-05 09:59:25 -0700

    adding firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,21 @@
-
+import { 
+  signInWithGooglePopup, 
+  createUserDocumentFromAuth 
+} from '../../utils/firebase/firebase.utils'
 
 
 const SignIn = () => {
-  return (
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    createUserDocumentFromAuth(user)
+  }
+
+   return (
     <div>
       <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>
+        Sign In With Google Popup
+      </button>
     </div>
   )
 }
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,47 @@
+import { initializeApp } from 'firebase/app';
+import { 
+  getAuth, 
+  signInWithRedirect, 
+  signInWithPopup, 
+  GoogleAuthProvider
+} from 'firebase/auth';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKbtLzOr7EHz-fBzVwfPkxUr-zbOihNx0",
+  authDomain: "crwn-db-be726.firebaseapp.com",
+  projectId: "crwn-db-be726",
+  storageBucket: "crwn-db-be726.appspot.com",
+  messagingSenderId: "979681399541",
+  appId: "1:979681399541:web:2eac82f13afa40bef86bbc"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account"
+});
+
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+}
+
