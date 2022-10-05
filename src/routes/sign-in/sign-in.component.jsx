7634cfc45d162a012b74ccfa67b10a59aadfd205
unknown <mbp818@gmail.com>
2022-10-05 15:35:07 -0700

    adding sign up form component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,21 +1,24 @@
 import { 
   signInWithGooglePopup, 
-  createUserDocumentFromAuth 
+  createUserDocumentFromAuth
 } from '../../utils/firebase/firebase.utils'
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
 
 const SignIn = () => {
-  const logGoogleUser = async () => {
+  
+ const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user)
-  }
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
+
 
    return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>
-        Sign In With Google Popup
-      </button>
+      <button onClick={logGoogleUser}>Sign In With Google</button>
+      <SignUpForm /> 
     </div>
   )
 }
