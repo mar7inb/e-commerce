a1b836f26cdcdab9a892148bb3d13576e0995085
unknown <mbp818@gmail.com>
2022-10-05 11:24:14 -0700

    signin component, firebase utils, and firebase libraries

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -43,5 +43,22 @@ export const createUserDocumentFromAuth = async (userAuth) => {
     const userSnapshot = await getDoc(userDocRef);
     console.log(userSnapshot);
     console.log(userSnapshot.exists());
-}
+
+    if(!userSnapshot.exists()) {
+      const {displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await setDoc(userDocRef, {
+          displayName,
+          email,
+          createdAt
+        });
+      } catch (err) {
+        console.log('err creating the user', err.message)
+      }
+    }
+
+    return userDocRef
+};
 
