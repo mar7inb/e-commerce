36736ed74b933dda23d85f85d05f775c56a1c288
unknown <mbp818@gmail.com>
2022-10-10 21:03:27 -0700

    created sign up component, form input component, and button comp

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react';
 
 import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component'
 
-import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth 
+} from '../../utils/firebase/firebase.utils';
+
+import './sign-up-form.styles.scss'
 
 const defaultFormFields = {
   displayName: '',
@@ -51,8 +55,9 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className='sign-up-container'>
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
          
          <FormInput 
@@ -90,7 +95,7 @@ const SignUpForm = () => {
            name="confirmPassword" 
            value={confirmPassword}
           />
-         <button type='submit'>Sign Up</button>
+         <Button buttonType='inverted' type='submit'>Sign Up</Button>
       </form>
     </div>
   )
