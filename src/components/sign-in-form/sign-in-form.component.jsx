18087e1906e00bcf9a2cbf553ba948aec1441366
unknown <mbp818@gmail.com>
2022-10-17 15:13:55 -0700

    fixing some stuff within components

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,9 +1,8 @@
-import { useState, useContext } from 'react';
+import { useState } from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component'
 
-import { UserContext } from '../../contexts/user.context';
 
 import { 
   signInWithGooglePopup, 
@@ -23,7 +22,6 @@ const SignInForm = () => {
   const [formFields, setFormFields ] = useState(defaultFormFields);
   const {  email, password } = formFields;
 
-  const { setCurrentUser } = useContext(UserContext);
 
 
   const resetFormFields = () => {
@@ -39,9 +37,7 @@ const SignInForm = () => {
      event.preventDefault();
 
      try{
-      const { user } = await signInAuthUserWithEmailAndPassword(email, password);
-      setCurrentUser(user);
-
+      await signInAuthUserWithEmailAndPassword(email, password);
       resetFormFields();
       } catch(error) {
         switch(error.code) {
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,22 +1,19 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom'
 
-import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
 
-import {signOutUser} from '../../utils/firebase/firebase.utils';
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+
+
 
 import './navigation.styles.scss'
 
 
 const Navigation = () => {
-  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const { currentUser} = useContext(UserContext);
   // console.log(currentUser);
-
-  const signOutHandler = async () => {
-    await signOutUser();
-    setCurrentUser(null);
-  }
 
 
   return (
@@ -31,7 +28,7 @@ const Navigation = () => {
           </Link>
           {
             currentUser ? (
-              <span className='nav-link' onClick={signOutHandler}>SIGN-OUT</span>
+              <span className='nav-link' onClick={signOutUser}>SIGN-OUT</span>
               ) : (
                <Link className='nav-link' to='/auth'>
               SIGN-IN
